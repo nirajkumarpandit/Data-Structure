0c6090f92a9df2c4851f83c503406d83f049c02e
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-01-21 17:43:19 +0530

    small change

--- Pattern/Flipped_simple_py.cpp
+++ Pattern/Flipped_simple_py.cpp
@@ -13,7 +13,7 @@ int main()
     cin >> n;
     for (int i = 0; i < n; i++)  // row
     {
-        for (int j = 0; j < n - i +-1; j++)   // space 
+        for (int j = 0; j < n - i +1; j++)   // space 
         {
             cout << " ";
         }
