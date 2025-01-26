dc122c2d25ff0b8476c11e56c431730467b2c82a
Niraj kumar <nirajkumarpandit45@gmail.com>
2025-01-26 23:43:38 +0530

    Pair sum

---
+++ Array/pairSumOPTA.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> pairSum(vector<int>nums, int target){
+    vector<int>ans;
+    int n=nums.size();
+    int i=0, j=n-1;
+    while(i<j){         // optimizied approach(OPTA) time complexity = O(n); linear time complexity
+        int pairSum=nums[i]+nums[j];
+        if(pairSum>target){
+            j--;
+        }
+       else if(pairSum<target){
+            i++;
+        }
+        else{
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+int main(){
+    vector<int>nums={2,7,11,15};
+   int  target=26;
+   vector<int>ans=pairSum(nums, target);
+   cout<<ans[0]<<" "<<ans[1];
+    return 0;
+}
